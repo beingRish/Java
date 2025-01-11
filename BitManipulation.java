bbb0f23c1c47939a18c2acc64a502a417fcf7910
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-01-11 17:07:15 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 
 public class BitManipulation {
-    public static void main(String[] args) {
 
-        // Get Bit
+    public static void getBit() {
+        // Get the 3rd bit (position = 2) of a number n. (n=0101)
         
         int n = 5; // 0101
         int pos = 3;
@@ -13,5 +14,60 @@ public class BitManipulation {
         } else {
             System.out.println("Bit was 1");
         }
+    }
+
+    public static void setBit() {
+        // set the 2nd bit (position = 1) of a numbr n. (n = 0101)
+
+        int n = 5; // 0101
+        int pos = 1;
+        int bitMask = 1 << pos;
+
+        int newNumber = bitMask | n;
+
+        System.out.println(newNumber);  // 7
+    }
+
+    public static void clearBit() {
+        // clear the 3rd bit (position = 2) of a numbr n. (n = 0101)
+
+        int n = 5; //0101
+        int pos = 2;
+        int bitMask = 1 << pos;
+        int notBitMast = ~(bitMask);
+
+        int newNumber = notBitMast & n;
+        System.out.println(newNumber);
+    }
+
+    public static void updateBit() {
+        // update the 2nd bit (position = 1) of a number n to oper. (n = 0101)
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1 or 0: ");
+        int oper = sc.nextInt();
+        // oper=1 : set, oper = 0 : clear
+        int n = 5; //0101
+        int pos = 1;
+
+        int bitMask = 1 << pos;
+        if(oper == 1) {
+            // set
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        } else {
+            // clear
+            int newBitMask = ~(bitMask);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // getBit();
+        // setBit();
+        // clearBit();
+        updateBit();
     }
 }
